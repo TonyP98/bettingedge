953e0c5de065ecd01a4e3a074e805ae9f08eb96e
TonyP98 <antonio.spina1998@gmail.com>
2025-09-09 13:35:52 +0200

    chore: pin deps, remove mlflow and update docs

--- engine/data/features.py
+++
@@ -1,9 +0,0 @@
-"""Feature engineering placeholders.
-
-This module will contain feature computation utilities in future PRs.
-"""
-
-
-def build_features(df):
-    """Placeholder function."""
-    return df
